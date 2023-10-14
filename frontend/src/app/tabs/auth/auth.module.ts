89b42a037f9be46319a552b01ceee041728abc22
Domagoj Burić <domagoj.dev@gmail.com>
2023-10-14 18:47:15 +0200

    [feature][YAC-3] Remove components

--- frontend/src/app/tabs/auth/auth.module.ts
+++ frontend/src/app/tabs/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { AuthComponent } from './auth/auth.component';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
     declarations: [AuthComponent],
-    imports: [CommonModule, IonicModule],
+    imports: [CommonModule, IonicModule, AuthRoutingModule],
     exports: [],
 })
 export class AuthModule {}
