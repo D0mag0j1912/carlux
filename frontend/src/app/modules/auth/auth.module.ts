0368cc2c5d357619e6240b52c02d19efebdd7729
Domagoj Burić <domagoj.dev@gmail.com>
2023-10-14 17:31:14 +0200

    [feature][YAC-3] Create auth module

---
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RegistrationComponent } from './registration/registration.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [RegistrationComponent],
+    imports: [CommonModule],
+    exports: [],
+})
+export class AuthModule {}
