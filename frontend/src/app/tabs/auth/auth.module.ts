9498f5ad0dd4779e817725f389414076d04d3aed
Domagoj Burić <domagoj.dev@gmail.com>
2023-10-14 18:04:44 +0200

    [feature][YAC-3] Update auth module placement

--- frontend/src/app/tabs/auth/auth.module.ts
+++ frontend/src/app/tabs/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RegistrationComponent } from './registration/registration.component';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
     declarations: [RegistrationComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, IonicModule],
     exports: [],
 })
 export class AuthModule {}
--- frontend/src/app/tabs/tabs-routing.module.ts
+++ frontend/src/app/tabs/tabs-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
             {
                 path: 'registration',
                 loadChildren: () =>
-                    import('../modules/auth/auth.module').then((module) => module.AuthModule),
+                    import('./auth/auth.module').then((module) => module.AuthModule),
             },
             {
                 path: 'tab2',
