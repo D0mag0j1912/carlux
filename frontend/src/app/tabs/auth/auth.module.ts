a248e09304db5d1db64c2b07daff24971113324a
Domagoj Burić <domagoj.dev@gmail.com>
2023-10-14 18:56:33 +0200

    [feature][YAC-3] Initialize auth store

--- frontend/src/app/tabs/auth/auth.module.ts
+++ frontend/src/app/tabs/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { AuthComponent } from './auth.component';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { AuthRoutingModule } from './auth-routing.module';
+import { StoreModule } from '@ngrx/store';
+import * as fromAuth from './reducers/index';
 
 @NgModule({
     declarations: [AuthComponent],
-    imports: [CommonModule, IonicModule, AuthRoutingModule],
+    imports: [
+        CommonModule,
+        IonicModule,
+        AuthRoutingModule,
+        StoreModule.forFeature('auth', fromAuth.reducers),
+    ],
     exports: [],
 })
 export class AuthModule {}
