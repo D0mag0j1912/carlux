ddfc2a609db663d8aa96cbe1b02892ddfbbc7b83
Domagoj Burić <domagoj.dev@gmail.com>
2023-10-16 18:00:53 +0200

    [feature][YAC-5] Translate tab button

--- frontend/src/app/tabs/tabs.module.ts
+++ frontend/src/app/tabs/tabs.module.ts
@@ -6,9 +6,10 @@ import { FormsModule } from '@angular/forms';
 import { TabsRoutingModule } from './tabs-routing.module';
 
 import { TabsComponent } from './tabs.component';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
-    imports: [IonicModule, CommonModule, FormsModule, TabsRoutingModule],
+    imports: [IonicModule, CommonModule, FormsModule, TabsRoutingModule, TranslocoModule],
     declarations: [TabsComponent],
 })
 export class TabsModule {}
