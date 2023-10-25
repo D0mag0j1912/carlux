ff45ae95e50de2e86a4507eb32f9e18336b98bbd
Domagoj Burić <domagoj.dev@gmail.com>
2023-10-25 19:38:02 +0200

    [feature][YAC-17] Import typeorm module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'Domagoj',
+            password: 'D0mag0j&Dev',
+            database: 'sailLux',
+            entities: [],
+            synchronize: true,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
