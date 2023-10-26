5b507d43202160aaab19b513624e28f19753cb82
Domagoj Burić <domagoj.dev@gmail.com>
2023-10-26 17:26:25 +0200

    [feature][YAC-17] Create user entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/auth/user.entity';
 
 @Module({
     imports: [
@@ -12,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
             username: 'Domagoj',
             password: 'D0mag0j&Dev',
             database: 'sailLux',
-            entities: [],
+            entities: [User],
             synchronize: true,
         }),
     ],
