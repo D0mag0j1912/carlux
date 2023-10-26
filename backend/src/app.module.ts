b6026a7290fb1af8ed05326a5fcf69546afe5718
Domagoj Burić <domagoj.dev@gmail.com>
2023-10-26 18:27:52 +0200

    [feature][YAC-17] Create .env file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,18 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './modules/auth/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'Domagoj',
-            password: 'D0mag0j&Dev',
-            database: 'sailLux',
-            entities: [User],
-            synchronize: true,
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get('DATABASE_HOST'),
+                port: configService.get('DATABASE_PORT'),
+                username: configService.get('DATABASE_USER'),
+                password: configService.get('DATABASE_PASSWORD'),
+                database: configService.get('DATABASE_NAME'),
+                entities: [User],
+                synchronize: configService.get('DATABASE_SYNCHRONIZE'),
+            }),
+            inject: [ConfigService],
         }),
     ],
     controllers: [AppController],
