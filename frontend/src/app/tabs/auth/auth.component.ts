7eecdec1728b722c19ca96851227349fbed9f1c1
Domagoj Burić <domagoj.dev@gmail.com>
2023-10-30 12:27:37 +0100

    [feature][YAC-12] Style auth form

--- frontend/src/app/tabs/auth/auth.component.ts
+++ frontend/src/app/tabs/auth/auth.component.ts
@@ -1,21 +1,30 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import * as intlTelInput from 'intl-tel-input';
 import { environment } from '../../../environments/environment';
+import { Platform } from '@ionic/angular';
 
 @Component({
     selector: 'yac-auth',
     templateUrl: './auth.component.html',
     styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent implements AfterViewInit {
+export class AuthComponent implements OnInit, AfterViewInit {
     form = new FormGroup({
         phoneNumber: new FormControl('', Validators.required),
     });
     readonly UTILS_SCRIPT = environment.utilsScript;
 
+    isDesktop = false;
+
     @ViewChild('phoneEl')
     phoneEl: ElementRef | undefined;
+
+    constructor(private _platform: Platform) {}
+
+    ngOnInit(): void {
+        this.isDesktop = this._platform.is('desktop');
+    }
 
     ngAfterViewInit(): void {
         if (this.phoneEl) {
