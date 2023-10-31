cce84c1b49040d75a2b77e3f718a826f881a71ab
Domagoj Burić <domagoj.dev@gmail.com>
2023-10-31 17:02:47 +0100

    [feature][YAC-19] Install nestjs swagger

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './modules/auth/user.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -33,7 +31,5 @@ import { TwilioModule } from 'nestjs-twilio';
             inject: [ConfigService],
         }),
     ],
-    controllers: [AppController],
-    providers: [AppService],
 })
 export class AppModule {}
--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
 
-@Controller()
-export class AuthController {}
+@ApiTags('Authentication')
+@Controller('api/auth')
+export class AuthController {
+    constructor(private _authService: AuthService) {}
+}
