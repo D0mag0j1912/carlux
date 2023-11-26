168276080e0f25bd1dbd253c77a268da1772a294
Domagoj Burić <domagoj.dev@gmail.com>
2023-11-26 17:23:52 +0100

    [feature][YAC-18] Add form fields

--- frontend/src/app/tabs/auth/components/personal-information-dialog/personal-information-dialog.component.ts
+++ frontend/src/app/tabs/auth/components/personal-information-dialog/personal-information-dialog.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
     standalone: true,
+    imports: [IonicModule, ReactiveFormsModule, TranslocoModule],
     templateUrl: './personal-information-dialog.component.html',
     styleUrls: ['./personal-information-dialog.component.scss'],
 })
-export class PersonalInformationDialogComponent {}
+export class PersonalInformationDialogComponent {
+    form = new FormGroup({
+        firstName: new FormControl('', [Validators.required]),
+        lastName: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+    });
+}
