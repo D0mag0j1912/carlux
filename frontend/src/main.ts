91d92f1aa8a919f00b3eb89349f9e4c482709695
Domagoj Burić <domagoj.dev@gmail.com>
2024-01-17 19:38:00 +0100

    [feature][YAC-30] Remove transloco root module

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,18 +5,19 @@ import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { provideIonicAngular } from '@ionic/angular/standalone';
+import { provideTransloco } from '@ngneat/transloco';
 import { environment } from './environments/environment';
 import { RootComponent } from './app/root.component';
 import { routes } from './app/app.routes';
 import { appReducers } from './app';
 import { SharedEffects } from './app/tabs/shared/shared-effects/shared.effects';
-import { TranslocoRootModule } from './app/transloco-root.module';
 import { ApiModule } from './app/api';
 import { AuthInterceptor } from './app/interceptors/auth.interceptor';
 import { PlatformModule } from './app/tabs/platform/platform.module';
 import * as PreferencesEffects from './app/tabs/preferences/effects/preferences.effects';
+import { TranslocoHttpLoader } from './app/transloco-loader';
 
 if (environment.production) {
     enableProdMode();
@@ -41,8 +42,6 @@ void bootstrapApplication(RootComponent, {
             }),
             StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
             EffectsModule.forRoot([SharedEffects, PreferencesEffects]),
-            HttpClientModule,
-            TranslocoRootModule,
             PlatformModule,
             ApiModule.forRoot({ rootUrl: environment.apiUrl }),
         ]),
@@ -52,5 +51,15 @@ void bootstrapApplication(RootComponent, {
             multi: true,
         },
         provideRouter(routes),
+        provideHttpClient(),
+        provideTransloco({
+            config: {
+                availableLangs: ['en', 'hr'],
+                defaultLang: 'en',
+                reRenderOnLangChange: true,
+                prodMode: environment.production,
+            },
+            loader: TranslocoHttpLoader,
+        }),
     ],
 });
