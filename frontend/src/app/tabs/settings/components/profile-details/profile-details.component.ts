ddc4cc146ba351dbe5189c589b54f4750b3ea33e
Domagoj Burić <domagoj.dev@gmail.com>
2024-01-21 16:34:26 +0100

    [feature][CAR-4] Remove unnecessary directive reference

--- frontend/src/app/tabs/settings/components/profile-details/profile-details.component.ts
+++ frontend/src/app/tabs/settings/components/profile-details/profile-details.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
+import {
+    IonBackButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+} from '@ionic/angular/standalone';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
     standalone: true,
+    imports: [
+        IonHeader,
+        IonToolbar,
+        IonButtons,
+        IonBackButton,
+        IonTitle,
+        IonContent,
+        TranslocoModule,
+    ],
     selector: 'car-profile-details',
     templateUrl: './profile-details.component.html',
     styleUrls: ['./profile-details.component.scss'],
