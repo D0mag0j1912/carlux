f38fd17dc01be1f5b1a45b4fd9f7b3693d67a416
Domagoj Burić <domagoj.dev@gmail.com>
2024-01-28 19:38:14 +0100

    [feature][CAR-4] Remove birth date field

--- frontend/src/app/tabs/settings/components/profile-details/profile-details.component.ts
+++ frontend/src/app/tabs/settings/components/profile-details/profile-details.component.ts
@@ -1,7 +1,6 @@
 import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
-import { ModalController } from '@ionic/angular';
 import {
     IonBackButton,
     IonButton,
@@ -20,19 +19,11 @@ import {
     IonText,
 } from '@ionic/angular/standalone';
 import { TranslocoModule } from '@ngneat/transloco';
-import { filter, from, take } from 'rxjs';
-import { format, parseISO } from 'date-fns';
-import { addIcons } from 'ionicons';
-import { calendarOutline } from 'ionicons/icons';
+import { filter, take } from 'rxjs';
 import { DateTimePickerComponent } from '../../../../shared/datetime-picker/datetime-picker.component';
-import { DATETIME_PICKER_INPUT_FORMAT } from '../../../../constants/datetime-picker-input-format';
 import { SettingsFacadeService } from '../../../../store/settings/facades/settings-facade.service';
 import { AuthenticationFacadeService } from '../../../auth/auth-facade.service';
 import { UserDto as User } from '../../../../api/models/user-dto';
-
-const DATE_FORMAT = 'yyyy-MM-dd';
-const PRESENTATION_DATE = 'MMM d, yyyy';
-const DATETIME_PICKER_CUSTOM_CLASS = 'datetime-picker';
 
 @Component({
     standalone: true,
@@ -59,20 +50,13 @@ const DATETIME_PICKER_CUSTOM_CLASS = 'datetime-picker';
     selector: 'car-profile-details',
     templateUrl: './profile-details.component.html',
     styleUrls: ['./profile-details.component.scss'],
-    providers: [ModalController],
 })
 export class ProfileDetailsComponent implements OnInit {
-    private _modalController = inject(ModalController);
     private _destroyRef = inject(DestroyRef);
     private _settingsFacadeService = inject(SettingsFacadeService);
     private _authenticationFacadeService = inject(AuthenticationFacadeService);
 
-    formattedTodayDate = signal<string>('');
     profileDetails = signal<User | undefined>(undefined);
-
-    constructor() {
-        addIcons({ calendarOutline });
-    }
 
     ngOnInit(): void {
         this._authenticationFacadeService
@@ -85,9 +69,6 @@ export class ProfileDetailsComponent implements OnInit {
             .pipe(filter(Boolean), takeUntilDestroyed(this._destroyRef))
             .subscribe((profileDetails: User | undefined) => {
                 this.profileDetails.set(profileDetails);
-                if (this.profileDetails()) {
-                    this._setFormattedDate(this.profileDetails()?.birthDate);
-                }
             });
     }
 
@@ -115,32 +96,5 @@ export class ProfileDetailsComponent implements OnInit {
             }
             return undefined;
         });
-    }
-
-    async openDateTimePicker(): Promise<void> {
-        const modal = await this._modalController.create({
-            component: DateTimePickerComponent,
-            componentProps: {
-                dateValue: format(new Date(), DATETIME_PICKER_INPUT_FORMAT),
-            },
-            cssClass: DATETIME_PICKER_CUSTOM_CLASS,
-        });
-        await modal.present();
-
-        from(modal.onDidDismiss<string | undefined>())
-            .pipe(takeUntilDestroyed(this._destroyRef))
-            .subscribe();
-    }
-
-    private _setFormattedDate(dateValue: string | undefined): void {
-        if (dateValue) {
-            const [date, time] = dateValue.split('T');
-            this.formattedTodayDate.set(
-                format(
-                    parseISO(format(new Date(date), DATE_FORMAT) + `T${time}`),
-                    PRESENTATION_DATE,
-                ),
-            );
-        }
     }
 }
