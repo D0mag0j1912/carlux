5a9cf4848819785c079a20e74f1893f5720200d4
Domagoj Burić <domagoj.dev@gmail.com>
2024-02-25 18:10:25 +0100

    [feature][CAR-8] Reverse query order

--- backend/src/modules/cars/cars.service.ts
+++ backend/src/modules/cars/cars.service.ts
@@ -25,28 +25,27 @@ export class CarsService {
         page: number,
         perPage: number,
     ): Promise<PaginationDto<RecommendedCarsDto>> {
-        const [recommendedCarsEntities, recommendedCarsTotalCount]: [CarEntity[], number] =
-            await this._carsRepository
-                .createQueryBuilder('car')
-                .leftJoin('car.currency', 'currency')
-                .leftJoin('car.images', 'image')
-                .select([
-                    'car.Id',
-                    'car.Brand',
-                    'car.KilometersTravelled',
-                    'car.Price',
-                    'car.FirstRegistrationDate',
-                    'car.ModelName',
-                    'car.CountryOrigin',
-                    'car.NoOfPreviousOwners',
-                    'car.UploadedDate',
-                    'currency.Symbol',
-                    'image.Image',
-                ])
-                .skip((page - 1) * perPage)
-                .take(perPage)
-                .orderBy('car.UploadedDate', 'DESC')
-                .getManyAndCount();
+        const [recommendedCarsEntities, recommendedCarsTotalCount] = await this._carsRepository
+            .createQueryBuilder('car')
+            .select([
+                'car.Id',
+                'car.Brand',
+                'car.KilometersTravelled',
+                'car.Price',
+                'car.FirstRegistrationDate',
+                'car.ModelName',
+                'car.CountryOrigin',
+                'car.NoOfPreviousOwners',
+                'car.UploadedDate',
+                'currency.Symbol',
+                'image.Image',
+            ])
+            .leftJoin('car.currency', 'currency')
+            .leftJoin('car.images', 'image')
+            .skip((page - 1) * perPage)
+            .take(perPage)
+            .orderBy('car.UploadedDate', 'DESC')
+            .getManyAndCount();
         const recommendedCars: RecommendedCarsDto[] = recommendedCarsEntities.map(
             (carEntity: CarEntity) => ({
                 id: carEntity.Id,
