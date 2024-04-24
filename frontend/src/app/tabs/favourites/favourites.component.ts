4fd3b02f1b53eebf010efcd7f8c2234295fe3af8
Domagoj Burić <domagoj.dev@gmail.com>
2024-04-24 20:20:12 +0200

    [feature][CAR-18] Add empty screen

--- frontend/src/app/tabs/favourites/favourites.component.ts
+++ frontend/src/app/tabs/favourites/favourites.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { IonContent, IonSpinner } from '@ionic/angular/standalone';
+import { TranslocoModule } from '@ngneat/transloco';
 import { FavouritesFacadeService } from '../../store/favourites/facades/favourites-facade.service';
 import { FavouriteCarItemComponent } from './favourite-car-item/favourite-car-item.component';
 
 @Component({
     standalone: true,
-    imports: [FavouriteCarItemComponent, IonSpinner, IonContent],
+    imports: [FavouriteCarItemComponent, IonSpinner, IonContent, TranslocoModule],
     templateUrl: './favourites.component.html',
     styleUrl: './favourites.component.scss',
 })
